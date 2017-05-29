ab72775b463ea078bf3392c2643d5ae8d18d3335
David Fisher <fisherds@rose-hulman.edu>
2017-05-29 14:59:18 -0400

    Added me some firebase

--- MovieQuotes/src/app/app.component.ts
+++ MovieQuotes/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase } from "angularfire2/database";
+import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database";
 
 interface MovieQuote {
   movie: string;
   quote: string;
+  $key?: string;
 }
 
 @Component({
@@ -12,27 +13,39 @@ interface MovieQuote {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  readonly quotesPath = "quotes";
+
   formMovieQuote: MovieQuote = {
     'quote': '',
     'movie': ''
   };
 
-  movieQuotes: Array<MovieQuote> = [
-    {"movie": "Rocky", "quote": "Yo Adrian"},
-    {"movie": "Terminator", "quote": "I'll be back"},
-    {"movie": "Titanic", "quote": "I'm the king of the world!"},
-    {"movie": "The Princess Bride", "quote": "Hello. My name is Inigo Montoya. You killed my father. Prepare to die."}
-  ];
+  // Local only solution
+  // movieQuotes: Array<MovieQuote> = [
+  //   {"movie": "Rocky", "quote": "Yo Adrian"},
+  //   {"movie": "Terminator", "quote": "I'll be back"},
+  //   {"movie": "Titanic", "quote": "I'm the king of the world!"},
+  //   {"movie": "The Princess Bride", "quote": "Hello. My name is Inigo Montoya. You killed my father. Prepare to die."}
+  // ];
+
+  movieQuotesStream: FirebaseListObservable<MovieQuote[]>;
 
   constructor(db: AngularFireDatabase) {
-
+    this.movieQuotesStream = db.list(this.quotesPath);
   }
 
   onSubmit(): void {
-    this.movieQuotes.unshift(this.formMovieQuote);
-    this.formMovieQuote = {
-      'quote': '',
-      'movie': ''
-    };
+    // Local only solution
+    // this.movieQuotes.unshift(this.formMovieQuote);
+    try {
+      this.movieQuotesStream.push(this.formMovieQuote);
+      this.formMovieQuote = {
+        'quote': '',
+        'movie': ''
+      };
+    } catch (e) {
+      console.log("Form error:", e);
+    }
   }
 }
